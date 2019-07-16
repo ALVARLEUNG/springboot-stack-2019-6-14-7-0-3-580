cb46dc5bddfbb7f3f2b8105cadda0889743c358a
ALVARLEUNG <alvar.leung@qq.com>
2019-07-16 16:18:16 +0800

    feat: add get all employees function

---
+++ src/main/java/com/tw/apistackbase/controller/EmployeesController.java
@@ -0,0 +1,22 @@
+package com.tw.apistackbase.controller;
+
+import com.tw.apistackbase.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeesController {
+
+
+    @GetMapping
+    public List<Employee> getEmployees () {
+        return Employee.createTestEmployees();
+    }
+
+    
+}
